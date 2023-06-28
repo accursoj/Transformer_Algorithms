001a8e01368b311ba254c69c1ad6759c677358bb
airat07 <102396976+airat07@users.noreply.github.com>
2023-06-28 13:48:28 -0400

    Update test.py

--- ETT_Datasets/Code/test.py
+++ ETT_Datasets/Code/test.py
@@ -3,59 +3,80 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score
   
-dataset = pd.read_csv('ETTh1_month1.csv')
+dataset = pd.read_csv('ETTm1_month1.csv')
 dataset
  
 X = dataset.iloc[:,1:2].values  
 y = dataset.iloc[:,7].values
- 
-# Linear Regression
-lin_model = LinearRegression()
-lin_model.fit(X,y)
-y_pred = lin_model.predict(X)
-# Calculate the R-squared value
-r_squared = r2_score(y, y_pred)
 
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Linear Regression model
+lin_model = LinearRegression()
+lin_model.fit(X_train, y_train)
+y_pred = lin_model.predict(X_train)
+
+# Calculate the R-squared value using the training set
+r_squared = r2_score(y_train, y_pred)
 print("R-squared value:", r_squared)
-# visualising the linear regression model
-plt.scatter(X,y, color='red')
-plt.plot(X, lin_model.predict(X),color='blue')
+
+# Visualizing Linear Regression
+X_grid = np.arange(min(X_train), max(X_train), 0.1).reshape(-1, 1)
+plt.scatter(X_train, y_train, color='red', label='Training data')
+plt.scatter(X_test, y_test, color='blue', label='Testing data')
+plt.plot(X_grid, lin_model.predict(X_grid), color='green', label='Linear regression')
 plt.title("OT change based on HUFL")
 plt.xlabel('High Useful Load')
 plt.ylabel('OT')
+plt.legend()
 plt.show()
 
 
- 
-# polynomial regression model
-from sklearn.preprocessing import PolynomialFeatures
+# Polynomial regression model
 poly_reg = PolynomialFeatures(degree=2)
-X_poly = poly_reg.fit_transform(X) 
-X_poly     # prints X_poly 
+X_poly = poly_reg.fit_transform(X_train)
 lin_model2 = LinearRegression()
-lin_model2.fit(X_poly,y)
+lin_model2.fit(X_poly, y_train)
 y_pred = lin_model2.predict(X_poly)
 
-# Calculate the R-squared value
-r_squared = r2_score(y, y_pred)
+# Calculate the R-squared value using the test set
+r_squared = r2_score(y_train, y_pred)
 print("R-squared value:", r_squared)
 
-# visualising polynomial regression
-from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree=4)
-X_poly = poly_reg.fit_transform(X)
-lin_model2 = LinearRegression()
-lin_model2.fit(X_poly,y)
-  
-X_grid = np.arange(min(X),max(X),0.1)
-X_grid = X_grid.reshape(len(X_grid),1) 
-plt.scatter(X,y, color='red') 
-  
-plt.plot(X_grid, lin_model2.predict(poly_reg.fit_transform(X_grid)),color='blue') 
-  
+# Visualizing polynomial regression
+X_grid = np.arange(min(X_train), max(X_train), 0.1).reshape(-1, 1)
+X_poly_grid = poly_reg.transform(X_grid)
+
+plt.scatter(X_train, y_train, color='red', label='Training data')
+plt.scatter(X_test, y_test, color='blue', label='Testing data')
+plt.plot(X_grid, lin_model2.predict(X_poly_grid), color='green', label='Polynomial regression')
 plt.title("OT change based on HUFL")
 plt.xlabel('High Useful Load')
 plt.ylabel('OT')
+plt.legend()
+plt.show()
+
+rf = RandomForestRegressor(n_estimators=100, random_state=42)
+rf.fit(X_train, y_train)
+y_pred = rf.predict(X_train)
+
+# Calculate the R-squared value using the training set
+r_squared = r2_score(y_train, y_pred)
+print("R-squared value:", r_squared)
+
+# Visualizing Random Forest regression
+X_grid = np.arange(min(X_train), max(X_train), 0.1).reshape(-1, 1)
+
+plt.scatter(X_train, y_train, color='red', label='Training data')
+plt.scatter(X_test, y_test, color='blue', label='Testing data')
+plt.plot(X_grid, rf.predict(X_grid), color='green', label='Random Forest regression')
+plt.title("OT change based on HUFL")
+plt.xlabel('High Useful Load')
+plt.ylabel('OT')
+plt.legend()
 plt.show()
